cb4654f68cb35c719243da55560c054483c42f71
parisath <parisath213@gmail.com>
2026-05-12 11:37:57 +0530

    the first message

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/1")
+def work():
+    return "hello"
+    return "how are you"
+
+@app.route("/workout")
+def vada():
+    return "workout polam"
